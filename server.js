dc8f45b9167ace28370f5192f20fd541cc0cf3fe
Alexey Zauzin <lehadnk@gmail.com>
2019-03-15 23:45:17 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+
+let client = new Discord.Client();
+client.login(process.env.BOT_TOKEN);
+
+let pingFab = function() {};
+client.on('ready', () => {
+
+    pingFab = () => {
+        let server = client.guilds.get('207912188407578624');
+        let channel = server.channels.get('211650891336515585');
+        channel.send('<@245197225943236619> ').then((msg) => {
+            msg.delete(100);
+        });
+    }
+
+});
+
+var http = require('http');
+http.createServer(function (req, res) {
+    pingFab();
+}).listen(8080);
